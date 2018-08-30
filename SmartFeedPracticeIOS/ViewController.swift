5a29c85da08f6575e04c6f02fbcceb8b534d0133
alonshp <alonshp1@gmail.com>
2018-08-30 13:22:23 +0300

    initialize project with pod

---
+++ SmartFeedPracticeIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartFeedPracticeIOS
+//
+//  Created by Alon Shprung on 8/30/18.
+//  Copyright © 2018 Alon Shprung. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
